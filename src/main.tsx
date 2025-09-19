d1cc4a5723202ea3921a0ccb172a5266bcbe4a79
LastBuff <jjjaaa1578@gmail.com>
2025-09-19 20:03:45 +0700

    v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import Grade from './component/Grade.tsx'
+/* import App from './App.tsx' */
+import Grade from './component/Grade/Grade.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
